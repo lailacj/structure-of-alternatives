ad3ff13638d30bff1a66b24ff8996cc599370357
Laila Johnston <lailajohnston@knights.ucf.edu>
2024-07-19 13:54:00 -0400

    added probability of a query in a set and getting a random set using bert

--- code/bert_alternatives_model.py
+++ code/bert_alternatives_model.py
@@ -1,11 +1,13 @@
 from transformers import BertTokenizer, BertForMaskedLM
+import numpy as np
 import pdb
+import random
 import torch
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-# ------ Getting next work predictions from BERT ------
+# ------ Getting next word predictions from BERT ------
 
 def get_top_k_predictions(text, top_k):
     inputs = tokenizer(text, return_tensors="pt")
@@ -34,6 +36,30 @@ def get_top_k_predictions(text, top_k):
     # Return the results as a list of (token, probability) tuples
     return list(zip(predicted_tokens, top_k_probs.tolist()))
 
+
+# ------ Set Model with BERT ------
+
+# The probability a query is in a set is equal to the probability of query being the next word. 
+def prob_query_in_set(top_k_predictions, query):
+    for token, prob in top_k_predictions:
+        if token == query:
+            return prob
+    
+    # Return None if the token is not in top_k_prediction
+    return None  
+
+def get_set(top_k_predictions, set_size=3):
+    words, probabilities = zip(*top_k_predictions)
+    
+    # Normalize probabilities to sum to 1
+    probabilities = np.array(probabilities)
+    probabilities /= probabilities.sum()
+
+    sampled_indices = np.random.choice(len(words), size=set_size, replace=False, p=probabilities)
+    sampled_words = [words[i] for i in sampled_indices]
+    
+    return sampled_words
+
 # Input text with [MASK] token
 text = "Sam opens the fridge and responds, 'I only have [MASK].'"
 top_k_size = 10
@@ -41,20 +67,10 @@ top_k_predictions = get_top_k_predictions(text, top_k_size)
 for token, prob in top_k_predictions:
     print(f'{token}: {prob:.4f}')
 
-# ------ Set Model with BERT ------
+query = "water"
+prob_query_set = prob_query_in_set(top_k_predictions, query)
+print(f"The probability of the token '{query}' is: {prob_query_set:.4f}" if prob_query_set else f"The token '{query}' is not in the top_k predictions.")
 
+set = get_set(top_k_predictions)
+print("Sampled set of words: ", set)
 
-
-# probabilities = torch.softmax(mask_token_logits, dim=-1)
-
-# # Get the top next word predicitions
-# top_k = 50
-# top_k_indices = torch.topk(probabilities, top_k).indices
-
-# # Decode the top 10 predictions and their probabilities
-# predicted_tokens = tokenizer.convert_ids_to_tokens(top_k_indices)
-# predicted_probs = probabilities[top_k_indices]
-
-# # Print the results
-# for token, prob in zip(predicted_tokens, predicted_probs):
-#     print(f'{token}: {prob.item():.4f}')
