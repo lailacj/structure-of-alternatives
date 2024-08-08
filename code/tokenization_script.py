0cc5b1f456d6351f41412442b022e480e193bde9
Laila Johnston <lailajohnston@knights.ucf.edu>
2024-08-08 14:37:47 -0400

    included numerical token value

--- code/tokenization_script.py
+++ code/tokenization_script.py
@@ -7,25 +7,15 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 df_experimental_data = pd.read_csv('../data/sca_dataframe.csv')
 
 def tokenize_words(words):
-    """
-    Tokenize a list of words using BERT tokenizer.
-    
-    Parameters:
-    words (list): List of words to tokenize.
-    
-    Returns:
-    list of str: List of tokenized words.
-    """
-
     tokenized_words = []
     for word in words:
         tokens = tokenizer.tokenize(word)
-        tokenized_words.append((word, tokens))
+        token_ids = tokenizer.convert_tokens_to_ids(tokens)
+        tokenized_words.append((word, tokens, token_ids))
     return tokenized_words
 
 words = df_experimental_data['query'].unique()
 tokenized_output = tokenize_words(words)
 
-# Print the tokenized output
-for word, tokens in tokenized_output:
-    print(f'Word: {word}, Tokens: {tokens}')
+for word, tokens, token_ids in tokenized_output:
+    print(f'Word: {word}, Tokens: {tokens}, Token IDs: {token_ids}')
