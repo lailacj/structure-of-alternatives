08d2113b22b605132c95d88c44cdfef8dd182179
Laila Johnston <lailajohnston@knights.ucf.edu>
2025-10-01 11:35:51 -0400

    cloze prob results

--- code/cloze_prob_model.py
+++ code/cloze_prob_model.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import numpy as np
+import pdb 
 
 SET_BOUNDARY_RANGE = range(2, 25, 1)
 NUM_SAMPLES = 100
@@ -33,7 +34,6 @@ common_contexts = set(df_experimental_data['story']).intersection(set(df_cloze['
 df_experimental_data = df_experimental_data[df_experimental_data['story'].isin(common_contexts)]
 df_cloze = df_cloze[df_cloze['context'].isin(common_contexts)]
 
-
 def sample_ordering(context:str, random_state) -> list[str]:
     """Sample an ordering of all words without replacement for a given context,
     weighted by cloze probability."""
@@ -50,16 +50,230 @@ def sample_ordering(context:str, random_state) -> list[str]:
     idx = rng.choice(len(words), size=len(words), replace=False, p=probs)
     return words[idx].tolist()
 
-demo = sample_ordering("fridge", random_state=123)
-print(demo)
+def ordering_log_likelihood(samples, query, trigger, query_negated):
+    count_query_above_trigger = 0
 
+    for sample in samples:
+        sampled_ordering = sample[0]
 
+        # do not need this try / except block if there is a good check of query and trigger in the log_likelihoods function
+        try:
+            query_index = sampled_ordering.index(query)
+        except ValueError:
+            query_index = -1 
+        try:
+            trigger_index = sampled_ordering.index(trigger)
+        except ValueError:
+            trigger_index = -1 
 
-# def results():
-#     ordering_data = []
-#     set_data = []
-#     conjunction_data = []
-#     disjunction_data = []
-#     always_negate_data = []
-#     never_negate_data = []
+        if query_index != -1 and trigger_index != -1 and query_index < trigger_index:
+            count_query_above_trigger += 1
 
+    empirical_probability = float(count_query_above_trigger) / float(len(samples))
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:   
+        log_likelihood = np.log(1e-10)
+
+    return log_likelihood, empirical_probability, prob_query_obs
+
+def set_log_likelihood(samples, query, query_negated):
+    count_query_in_set = 0
+
+    for sample in samples:
+        sampled_set = sample[1]
+
+        if query in sampled_set:
+            count_query_in_set += 1
+
+    empirical_probability = float(count_query_in_set) / float(len(samples))
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:
+        log_likelihood = np.log(1e-10)
+    
+    return log_likelihood, empirical_probability, prob_query_obs
+
+def conjunction_log_likelihood(samples, query, trigger, query_negated):
+    count_conjunction = 0
+
+    for sample in samples:
+        sampled_ordering = sample[0]
+        sampled_set = sample[1]
+
+        if query in sampled_set:
+            query_index = sampled_ordering.index(query)
+            trigger_index = sampled_ordering.index(trigger)
+            if query_index < trigger_index:
+                count_conjunction += 1
+
+    empirical_probability = float(count_conjunction) / float(len(samples))
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:  
+        log_likelihood = np.log(1e-10)
+    
+    return log_likelihood, empirical_probability, prob_query_obs
+
+def disjunction_log_likelihood(samples, query, trigger, query_negated):
+    count_disjunction = 0
+
+    for sample in samples:
+        sampled_ordering = sample[0]
+        sampled_set = sample[1]
+
+        query_index = sampled_ordering.index(query)
+        trigger_index = sampled_ordering.index(trigger)
+        
+        if ((query in sampled_set) or (query_index < trigger_index)):
+            count_disjunction += 1
+
+    empirical_probability = float(count_disjunction) / float(len(samples))
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:   
+        log_likelihood = np.log(1e-10)
+    
+    return log_likelihood, empirical_probability, prob_query_obs
+
+def always_negate_log_likelihood(query_negated):
+    empirical_probability = 1
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:   
+        log_likelihood = np.log(1e-10)
+    
+    return log_likelihood, empirical_probability, prob_query_obs
+    
+def never_negate_log_likelihood(query_negated):
+    empirical_probability = 0
+
+    if query_negated == 1:
+        prob_query_obs = empirical_probability
+    else:
+        prob_query_obs = 1 - empirical_probability
+
+    if prob_query_obs > 0:
+        log_likelihood = np.log(prob_query_obs)
+    else:   
+        log_likelihood = np.log(1e-10)
+    
+    return log_likelihood, empirical_probability, prob_query_obs
+
+def results():
+    ordering_data = []
+    set_data = []
+    conjunction_data = []
+    disjunction_data = []
+    always_negate_data = []
+    never_negate_data = []
+
+    contexts = df_experimental_data['story'].unique()
+    context_samples = {}
+
+    for set_boundary in SET_BOUNDARY_RANGE:
+
+        # Get sampled orderings and sets for each context
+        for context in contexts:
+            context_samples[context] = []
+
+            for i in range(0, NUM_SAMPLES):
+                sampled_ordering = sample_ordering(context, random_state=i)
+                sampled_set = sampled_ordering[:set_boundary]
+                # pdb.set_trace()
+                context_samples[context].append((sampled_ordering, sampled_set))
+
+        # Iterate through the experimental data
+        for _, row in df_experimental_data.iterrows():
+            context = row['story']
+            query = row['cleaned_query']
+            trigger = row['cleaned_trigger']
+            query_negated = row['neg']
+
+            words_in_context = set(df_cloze.loc[df_cloze["context"] == context, "word"])
+            
+            if query not in words_in_context:
+                continue  
+
+            if trigger not in words_in_context:
+                continue      
+
+            # For each trial, compute log likelihoods across all samples
+            samples = context_samples[context]
+            ordering_log_likelihood_single_trial, ordering_empirical_p, ordering_prob_query_obs = ordering_log_likelihood(samples, query, trigger, query_negated)
+            set_log_likelihood_single_trial, set_empirical_p, set_prob_query_obs = set_log_likelihood(samples, query, query_negated)
+            conjunction_log_likelihood_single_trial, conjunction_empirical_p, conjunction_prob_query_obs = conjunction_log_likelihood(samples, query, trigger, query_negated)
+            disjunction_log_likelihood_single_trial, disjunction_empirical_p, disjunction_prob_query_obs = disjunction_log_likelihood(samples, query, trigger, query_negated)
+            always_negate_log_likelihood_single_trial, always_negate_empirical_p, always_negate_prob_query_obs = always_negate_log_likelihood(query_negated)
+            never_negate_log_likelihood_single_trial, never_negate_empirical_p, never_negate_prob_query_obs = never_negate_log_likelihood(query_negated)
+
+            ordering_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, ordering_log_likelihood_single_trial, ordering_empirical_p, ordering_prob_query_obs])
+            set_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, set_log_likelihood_single_trial, set_empirical_p, set_prob_query_obs])
+            conjunction_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, conjunction_log_likelihood_single_trial, conjunction_empirical_p, conjunction_prob_query_obs])
+            disjunction_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, disjunction_log_likelihood_single_trial, disjunction_empirical_p, disjunction_prob_query_obs])
+            always_negate_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, always_negate_log_likelihood_single_trial, always_negate_empirical_p, always_negate_prob_query_obs])
+            never_negate_data.append([set_boundary, NUM_SAMPLES, context, trigger, query, query_negated, never_negate_log_likelihood_single_trial, never_negate_empirical_p, never_negate_prob_query_obs])
+
+        # Convert lists to DataFrames
+        set_df = pd.DataFrame(set_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+        ordering_df = pd.DataFrame(ordering_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+        conjunction_df = pd.DataFrame(conjunction_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+        disjunction_df = pd.DataFrame(disjunction_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+        always_negate_df = pd.DataFrame(always_negate_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+        never_negate_df = pd.DataFrame(never_negate_data, columns=['set_boundary', 'num_samples', 'context', 'trigger', 'query', 'neg', 'log_likelihood', 'empirical_probability', 'probability_query_observed'])
+
+        # Save results to CSV incrementally
+        set_df.to_csv('../results/set_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/set_results_cloze_prob_sep2025.csv'), index=False)
+        ordering_df.to_csv('../results/ordering_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/ordering_results_cloze_prob_sep2025.csv'), index=False)
+        conjunction_df.to_csv('../results/conjunction_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/conjunction_results_cloze_prob_sep2025.csv'), index=False)
+        disjunction_df.to_csv('../results/disjunction_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/disjunction_results_cloze_prob_sep2025.csv'), index=False)
+        always_negate_df.to_csv('../results/always_negate_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/always_negate_results_cloze_prob_sep2025.csv'), index=False)
+        never_negate_df.to_csv('../results/never_negate_results_cloze_prob_sep2025.csv', mode='a', header=not pd.io.common.file_exists('../results/never_negate_results_cloze_prob_sep2025.csv'), index=False)
+
+        print(set_df.head())
+        print(ordering_df.head())
+        print(conjunction_df.head())
+        print(disjunction_df.head())
+
+        # Clear lists to free memory
+        set_data.clear()
+        ordering_data.clear()
+        conjunction_data.clear()
+        disjunction_data.clear()
+        always_negate_data.clear()
+        never_negate_data.clear()
+
+    return None
+
+print("Starting log likelihood calculations...")
+results()
--- code/word_freq_to_cloze_prob.py
+++ code/word_freq_to_cloze_prob.py
@@ -3,14 +3,19 @@
 import pandas as pd
 import ast
 
-# Load word frequencies file
-word_freq_df = pd.read_csv("../data/generation_task_word_frequencies.csv")
-
-#number of participants in generation task
+# ---- Config ----
 N_PARTICIPANTS = 52
+NEG_ALPHA = 1-(1/N_PARTICIPANTS)  # scale factor to keep neg probs strictly below the min positive prob
+INPUT_PATH = "../data/generation_task_word_frequencies.csv"
+OUTPUT_PATH = "../data/word_freq_and_cloze_prob.csv"
+
+# ---- Load ----
+word_freq_df = pd.read_csv(INPUT_PATH)
 
 rows = []
+context_min_pos_prob = {}  # context -> lowest positive cloze prob
 
+# ---------- PASS 1: POS columns ----------
 for col in word_freq_df.columns:
     if col.endswith("_pos"):
         context = col.replace("_pos", "")
@@ -19,9 +24,69 @@ for col in word_freq_df.columns:
             # Parse string "(word, freq)" safely
             word, freq = ast.literal_eval(cell)
             cloze_prob = freq / N_PARTICIPANTS
-            rows.append([context, word, freq, cloze_prob])
+            rows.append([context, word, freq, cloze_prob, "pos"])
 
-output_df = pd.DataFrame(rows, columns=["context", "word", "frequency", "cloze_probability"])
+            # track min positive cloze prob for this context
+            if context not in context_min_pos_prob:
+                context_min_pos_prob[context] = cloze_prob
+            else:
+                context_min_pos_prob[context] = min(context_min_pos_prob[context], cloze_prob)
+
+# ---------- PASS 2: NEG columns ----------
+for col in word_freq_df.columns:
+    if col.endswith("_neg"):
+        context = col.replace("_neg", "")
+
+        # If no positive column was found for this context, skip safely
+        # (or set a conservative default)
+        if context not in context_min_pos_prob:
+            # You could set a tiny cap if you want to include these:
+            # context_min_pos_prob[context] = 1.0 / N_PARTICIPANTS
+            # For now, skip to be explicit:
+            continue
+
+        # Collect all (word, freq) for this neg column
+        neg_items = []
+        for cell in word_freq_df[col].dropna():
+            word, freq = ast.literal_eval(cell)
+            neg_items.append((word, freq))
+
+        if not neg_items:
+            continue
+
+        total_neg_freq = sum(f for _, f in neg_items)
+
+        # If there’s no mass, assign zero
+        if total_neg_freq == 0:
+            for word, freq in neg_items:
+                rows.append([context, word, freq, 0.0, "neg"])
+            continue
+
+        # cap under the min positive cloze prob
+        cap = context_min_pos_prob[context] * NEG_ALPHA
+
+        # Distribute proportionally so ordering is preserved
+        for word, freq in neg_items:
+            score = freq / total_neg_freq
+            cloze_prob = score * cap
+            # Make sure it's strictly below the min positive (guard floating point)
+            cloze_prob = min(cloze_prob, context_min_pos_prob[context] - 1e-12)
+            rows.append([context, word, freq, cloze_prob, "neg"])
+
+# ---------- Build output ----------
+output_df = pd.DataFrame(
+    rows, columns=["context", "word", "frequency", "cloze_probability", "type"]
+)
+
+# Remove NEG duplicates if the same word exists in POS for the same context
+output_df = output_df[
+    ~(
+        (output_df["type"] == "neg") &
+        (output_df.set_index(["context", "word"]).index.isin(
+            output_df[output_df["type"] == "pos"].set_index(["context", "word"]).index
+        ))
+    )
+]
 
 # Rename contexts 
 rename_map = {
@@ -37,5 +102,5 @@ output_df["context"] = output_df["context"].replace(rename_map)
 # Remove all rows with context = "play"
 output_df = output_df[output_df["context"] != "play"]
 
-output_path = "../data/word_freq_and_cloze_prob.csv"
-output_df.to_csv(output_path, index=False)
+# Save
+output_df.to_csv(OUTPUT_PATH, index=False)
