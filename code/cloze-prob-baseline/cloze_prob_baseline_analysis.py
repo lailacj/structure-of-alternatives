ec13ab08916926a08953c762356b5aa4d337e330
ljohnst7 <laila_johnston@brown.edu>
2026-02-04 18:17:56 -0500

    cloze probability baseline

---
+++ code/cloze-prob-baseline/cloze_prob_baseline_analysis.py
@@ -0,0 +1,15 @@
+# Cloze prob baseline model. 
+# This code gets plots. 
+
+import pandas as pd
+
+# ------------------------------
+# Paths 
+# ------------------------------
+
+SET_RESULTS = pd.read_csv('../../results/cloze_prob_baseline/set_results_cloze_prob_sep2025.csv')
+ORDERING_RESULTS = pd.read_csv('../../results/cloze_prob_baseline/ordering_results_cloze_prob_sep2025.csv')
+CONJUNCTION_RESULTS = pd.read_csv('../../results/cloze_prob_baseline/conjunction_results_cloze_prob_sep2025.csv')
+DISJUNCTION_RESULTS = pd.read_csv('../../results/cloze_prob_baseline/disjunction_results_cloze_prob_sep2025.csv')
+
+
